73b8fc8724cf8d2bc636b7d72bd61d38798dd974
SamyakJain2020 <samyakjainking@gmail.com>
2022-08-09 02:16:07 +0530

    package.json changes

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,25 @@ var express = require("express");
 var ethers = require("ethers");
 var router = express.Router();
 
-const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
-const contractAddress = "0x8d12a197cb00d4747a1fe03395095ce2a5cc6819";
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://matic-mumbai.chainstacklabs.com"
+);
+const matic = new ethers.Contract("0x8d12a197cb00d4747a1fe03395095ce2a5cc6819");
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
 // routher to get the balance of the user
-router.get("/balance", function (req, res, next) {
-  var wallet = new ethers.Wallet("0x" + process.env.PRIVATE_KEY, provider);
-  wallet.getBalance().then(function (balance) {
+router.get("/balance", async function (req, res, next) {
+  try {
+    const address = "0x8d12a197cb00d4747a1fe03395095ce2a5cc6819";
+    const balance = await provider.getBalance(address);
     res.send(balance.toString());
-  });
-});
-// router to connect to the contract
-router.get("/contract", function (req, res, next) {
-  var wallet = new ethers.Wallet("0x" + process.env.PRIVATE_KEY, provider);
-  var contract = new ethers.Contract(contractAddress, contractABI, wallet);
-  contract.getBalance().then(function (balance) {
-    res.send(balance.toString());
-  });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 module.exports = router;
